009c0aab260a1d1b4586815609c44908a1d7fcce
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-15 14:04:47 -0500

    commit tree 2ee4ce32ccc09005df794d5851e1332cd80a4764

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if{|flavor| flavor == "strawberry"}
+          binding.pry
 
+        end
+      end
+    end
+  end
 end
